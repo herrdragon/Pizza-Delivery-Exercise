49aaa98a2965aa6861fe5d1898b985c807f8dbc2
Miguel Herrnsdorf <herrdragon@users.noreply.github.com>
2022-02-13 17:28:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const args = process.argv;
+const commands = ['read'];
+
+const getHelp = function() {
+    const helpText = `
+    Simple node program to handle files using streams.
+    usage:
+        node index.js <command> <path_to_file>
+
+        <command> can be:
+        read: Parse file's pizza order for delivery
+
+        <path_to_file> is the path to the file you want to work with.
+    `;
+    console.log(helpText);
+}
+const read = (filePath) => {
+    const readableStream = fs.createReadStream(filePath, 'utf8');
+    let map = new Map();
+    let x = y = 0;
+    // House at 00 get one pizza from the start
+    map.set(`${x}${y}`, 1)
+    readableStream.on('error', function (error) {
+        console.log(`error: ${error.message}`);
+    })
+    
+    readableStream.on('data', (steps) => {
+        for(let step of steps) {
+            if(step == '>')
+                x++;
+            if(step == '<')
+                x--;
+            if(step == '^')
+                y++;
+            if(step == 'v')
+                y--;
+            person.deliver(map, x, y);
+            // console.log(step, x, y);
+        }
+    })
+
+    readableStream.on('end', () => {
+        // for (const [key, value] of map) {
+        //     console.log(key + ' = ' + value)
+        // }
+        console.log('Houses received at least one pizza', map.size)
+        let uno = dos = more = 0;
+        for (const value of map.values()) {
+            if(value == 1)
+                uno++;
+            if(value == 2)
+                dos++;
+            if(value > 2)
+                more++;
+        }
+        console.log('Houses received one pizza', uno)
+        console.log('Houses received two pizzas', dos)
+        console.log('Houses received more than two pizzas', more)
+    })
+}
+class Delivery {
+    deliver (map, x, y) {
+        if(map.get(`${x}${y}`) == undefined)
+            map.set(`${x}${y}`, 1);
+        else
+            map.set(`${x}${y}`, map.get(`${x}${y}`)+1);
+    }
+}
+const person = new Delivery();
+
+let command = '';
+
+if(args.length < 3) {
+    getHelp();
+    return;
+}
+else if(args.length > 4) {
+    console.log('More arguments provided than expected');
+    getHelp();
+    return;
+}
+else {
+    command = args[2]
+    if(!args[3]) {
+        console.log('This tool requires at least one path to a file');
+        getHelp();
+        return;
+    }
+}
+
+switch(commands.indexOf(command)) {
+    case 0:
+        read(args[3]);
+        break;
+    default:
+        console.log('You entered a wrong command. See help text below for supported functions');
+        getHelp();
+        return;
+}
+
+
